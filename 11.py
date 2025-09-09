0cd31971ccb59c416273edf3d2608ee0c7bb1dd4
Niraj kumar <nk8384546@gmail.com>
2025-09-09 15:06:46 +0530

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,1 +1,13 @@
+x=[[2,3,4],
+   [5,6,8],
+    [6,7,3]]
 
+y=[[8,9,7],
+    [3,4,5],
+    [7,6,8]]
+add=[[x[i][j]+y[i][j]
+      for j in range (len (x[0]))]
+      for i in range (len (x))]
+print ("addition of matrix:")
+for row in add:
+    print(row)
